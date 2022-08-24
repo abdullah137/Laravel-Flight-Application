bc88f9cc69dfaf7ebedef7f0efc7a68846f6c437
Abdullah <abdullahyahaya46@gmail.com>
2022-08-24 15:39:50 +0100

    Modified the User Migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('middleName')->nullable();
+            $table->string('nationality');
+            $table->string('gender');
+            $table->string('dob');
+            $table->string('address');
+            $table->string('phoneNumber');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
